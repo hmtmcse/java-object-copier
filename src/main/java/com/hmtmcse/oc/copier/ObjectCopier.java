2725a4937d4be4347bc184bd9e34a8ecb63910ac
Touhid Mia <hmtm.cse@gmail.com>
2020-07-24 12:45:35 +0600

    Object Copier

--- src/main/java/com/hmtmcse/oc/copier/ObjectCopier.java
+++ src/main/java/com/hmtmcse/oc/copier/ObjectCopier.java
@@ -4,6 +4,18 @@ import java.lang.reflect.InvocationTargetException;
 
 public class ObjectCopier {
 
+    public Boolean isValidateSource = true;
+    public Boolean isValidateDestination = true;
+
+    public ObjectCopier setValidateSource(Boolean validateSource) {
+        isValidateSource = validateSource;
+        return this;
+    }
+
+    public ObjectCopier setValidateDestination(Boolean validateDestination) {
+        isValidateDestination = validateDestination;
+        return this;
+    }
 
     public <D> D copy(Object source, Class<D> destination) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         return destination.getDeclaredConstructor().newInstance();
