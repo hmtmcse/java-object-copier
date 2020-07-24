f32b60c5db375a6fa54a0db587fd21564947cabd
Touhid Mia <hmtm.cse@gmail.com>
2020-07-24 12:02:29 +0600

    Object Copier

--- src/main/java/com/hmtmcse/oc/copier/ObjectCopier.java
+++ src/main/java/com/hmtmcse/oc/copier/ObjectCopier.java
@@ -1,4 +1,12 @@
 package com.hmtmcse.oc.copier;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class ObjectCopier {
+
+
+    public <D> D copy(Object source, Class<D> destination) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        return destination.getDeclaredConstructor().newInstance();
+    }
+
 }
