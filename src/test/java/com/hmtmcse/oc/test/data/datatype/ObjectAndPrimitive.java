be365d7da793b75a083fdf60f356adebf82bb9ee
Touhid Mia <hmtm.cse@gmail.com>
2020-08-03 16:20:45 +0600

    Copy Test

--- src/test/java/com/hmtmcse/oc/test/data/datatype/ObjectAndPrimitive.java
+++ src/test/java/com/hmtmcse/oc/test/data/datatype/ObjectAndPrimitive.java
@@ -3,6 +3,8 @@ package com.hmtmcse.oc.test.data.datatype;
 import com.hmtmcse.oc.test.data.Gender;
 import com.hmtmcse.oc.test.data.Student;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -11,16 +13,20 @@ public class ObjectAndPrimitive {
 
     public String stringType;
     public Integer integerType;
+    public Short shortType;
     public Long longType;
     public Double doubleType;
     public Float floatType;
-    public Gender enumType;
-    public Student objectType;
     public Byte byteType;
+    public Boolean booleanType;
+    public Character character;
+
     public Date dateType;
     public LocalDate localDateType;
     public LocalDateTime localDateTimeType;
-    public Boolean booleanType;
+
+    public BigDecimal bigDecimal;
+    public BigInteger bigInteger;
 
     public byte aByte;
     public short aShort;
