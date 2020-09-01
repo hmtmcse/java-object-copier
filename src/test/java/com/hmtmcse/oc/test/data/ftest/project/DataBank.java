6289fdf79ef51498f4ca9b61d2a817975e96fed5
Touhid Mia <hmtm.cse@gmail.com>
2020-09-01 23:21:44 +0600

    Test Cases

--- src/test/java/com/hmtmcse/oc/test/data/ftest/project/DataBank.java
+++ src/test/java/com/hmtmcse/oc/test/data/ftest/project/DataBank.java
@@ -8,8 +8,8 @@ import java.util.UUID;
 public class DataBank {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++){
-            System.out.println(UUID.randomUUID().toString().toUpperCase());
+        for (int i = 0; i < 1000; i++){
+            System.out.println(UUID.randomUUID().toString());
         }
     }
 
